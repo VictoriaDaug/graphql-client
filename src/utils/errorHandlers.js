3f2f527019d1a563599606ebadc4349f6fe7ae4c
Victoria Daug <victoria.daug@acresoftware.com>
2019-10-24 12:03:47 +0100

    Add gql Client and error handling

---
+++ src/utils/errorHandlers.js
@@ -0,0 +1,52 @@
+import {
+    GraphQLError
+} from 'graphql'
+
+const rehydrateGraphQLError = error => {
+    if (typeof error === 'string') {
+        return new GraphQLError(error);
+    } else if (typeof error === 'object' && error.message) {
+        return new GraphQLError(
+            error.message,
+            error.nodes,
+            error.source,
+            error.positions,
+            error.path,
+            error.originalError,
+            error.extensions || {}
+        );
+    }
+}
+
+const makeErrorMessage = (networkError, graphQLErrors) => {
+    if (networkError) {
+        return '[Network Error] ' + networkError.message;
+    } else if (graphQLErrors) {
+        return graphQLErrors
+            .map(({
+                message
+            }) => `[GraphQL Error] ${message}`)
+            .join('\n');
+    } else {
+        return '[CombinedError]';
+    }
+};
+
+export class CombinedError extends Error {
+    constructor({
+        networkError,
+        graphQLErrors
+    }) {
+        const rehydratedErrors = graphQLErrors ?
+            graphQLErrors.map(rehydrateGraphQLError) :
+            undefined;
+        const message = makeErrorMessage(networkError, rehydratedErrors);
+
+        super(message);
+
+        this.name = 'CombinedError';
+        this.message = message;
+        this.graphQLErrors = rehydratedErrors;
+        this.networkError = networkError;
+    }
+}
